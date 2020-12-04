93e2867b0c125072ab04a1afba4466b7ae6f12e3
Igor_Lenko <mk2008@mail.ru>
2020-12-04 13:24:31 +0300

    update_css

--- script/index.js
+++ script/index.js
@@ -5,52 +5,54 @@ const button = document.querySelector("#submit"),
       regexp = /^\s*$/;
 let inputValue = {};
 
-
-input.addEventListener("focus", () =>{
-  text.style.background = "";
-  text.innerHTML = "";
-  button.disabled = false;
-})
-
-button.addEventListener("click", () => {
-    if(regexp.test(input.value)){
-      text.style.color = "red";
-      text.innerHTML = "This input is not correct. Please again.";
-      input.value = "";
-    }else{
-      inputValue.number = input.value;
-
-      sendRequest("POST", requestURL, inputValue)
-        .then(() => {
-          button.disabled = true;
-          input.value = "";
-          text.style.color = "green";
-          text.style.background = "#ffffffb5";
-          text.innerHTML = "Your order is accepted";
-        })
-        .catch(err => console.log(err))
-    }
-})
-
-function sendRequest(method, url, body){
-  return fetch(url, {
-    method : method,
-    body : JSON.stringify(body),
-    headers: {"Content-Type" : "application/json"}
+window.onload = ()=>{
+  input.addEventListener("focus", () =>{
+    text.style.background = "";
+    text.innerHTML = "";
+    button.disabled = false;
   })
-    .then(respons => {
-      if(respons.ok){
-        respons.json();
+  
+  button.addEventListener("click", () => {
+      if(regexp.test(input.value)){
+        text.style.color = "red";
+        text.innerHTML = "This input is not correct. Please again.";
+        input.value = "";
       }else{
-        return respons.json()
-          .then(error => {
-            const e = new Error("Ошибка");
-            e.data = error;
-            throw e;
+        inputValue.number = input.value;
+  
+        sendRequest("POST", requestURL, inputValue)
+          .then(() => {
+            button.disabled = true;
+            input.value = "";
+            text.style.color = "green";
+            text.style.background = "#ffffffb5";
+            text.innerHTML = "Your order is accepted";
           })
+          .catch(err => console.log(err))
       }
+  })
+  
+  function sendRequest(method, url, body){
+    return fetch(url, {
+      method : method,
+      body : JSON.stringify(body),
+      headers: {"Content-Type" : "application/json"}
     })
+      .then(respons => {
+        if(respons.ok){
+          respons.json();
+        }else{
+          return respons.json()
+            .then(error => {
+              const e = new Error("Ошибка");
+              e.data = error;
+              throw e;
+            })
+        }
+      })
+  }
 }
+
 
 
 
